0fcfa678c07d743ec687f6f04168bd84cecb09cd
Maxthon72 <maciekageof@gmail.com>
2023-09-25 14:39:36 +0200

    Staring configuration of project

---
+++ FundRipple/src/main/java/com/fundripple/fundripple/FundRippleApplication.java
@@ -0,0 +1,13 @@
+package com.fundripple.fundripple;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FundRippleApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(FundRippleApplication.class, args);
+	}
+
+}
---
+++ FundRipple/src/test/java/com/fundripple/fundripple/FundRippleApplicationTests.java
@@ -0,0 +1,13 @@
+package com.fundripple.fundripple;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FundRippleApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
