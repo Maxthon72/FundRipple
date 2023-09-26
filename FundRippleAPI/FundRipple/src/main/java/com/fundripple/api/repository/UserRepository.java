c409651b01163fc889ecf10dac7ea3300d988ed0
Maxthon72 <maciekageof@gmail.com>
2023-09-26 14:00:27 +0200

    Spring security JWT, starting work

--- FundRippleAPI/FundRipple/src/main/java/com/fundripple/api/repository/UserRepository.java
+++ FundRippleAPI/FundRipple/src/main/java/com/fundripple/api/repository/UserRepository.java
@@ -3,6 +3,10 @@ package com.fundripple.api.repository;
 import com.fundripple.api.model.entity.User;
 import org.springframework.data.jpa.repository.JpaRepository;
 
+import java.util.Optional;
+
 public interface UserRepository extends JpaRepository<User, Long> {
-    // You can add custom query methods here if needed
+
+    Optional<User> findByEmail(String email);
+
 }
