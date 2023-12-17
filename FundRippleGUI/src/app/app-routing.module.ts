c66142282a20bce7462bc372e8e008820883f489
Maxthon72 <maciekageof@gmail.com>
2023-12-17 18:59:29 +0100

    Configuring routing

--- FundRippleGUI/src/app/app-routing.module.ts
+++ FundRippleGUI/src/app/app-routing.module.ts
@@ -4,13 +4,21 @@ import { HomePageComponent } from './components/home-page/home-page.component';
 import { LoginPageComponent } from './components/login-page/login-page.component';
 import { RegisterPageComponent } from './components/register-page/register-page.component';
 import { CreateProjectComponent } from './components/create-project/create-project.component';
+import { SearchProjectComponent } from './components/search-project/search-project.component';
+import { SuspicionsProjectComponent } from './components/suspicions-project/suspicions-project.component';
+import { VerifyProjectComponent } from './components/verify-project/verify-project.component';
+import { SpecificProjectComponent } from './components/specific-project/specific-project.component';
 
 const routes: Routes = [
-  {path: 'home', component:HomePageComponent},
-  { path: '', redirectTo: '/home', pathMatch: 'full' },
-  {path: 'login', component:LoginPageComponent},
-  {path: 'register', component:RegisterPageComponent},
-  {path:'createProject',component:CreateProjectComponent}
+  {path:'home', component:HomePageComponent},
+  {path:'', redirectTo: '/home', pathMatch: 'full' },
+  {path:'login', component:LoginPageComponent},
+  {path:'register', component:RegisterPageComponent},
+  {path:'createProject',component:CreateProjectComponent},
+  {path:'search',component:SearchProjectComponent},
+  {path:'suspicions',component:SuspicionsProjectComponent},
+  {path:'verify',component:VerifyProjectComponent},
+  {path:'project/:projectName',component:SpecificProjectComponent}
 ];
 
 @NgModule({
